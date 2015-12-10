edcc2e40fa40eb22dd9063e435afa233725e7363
Brendan Whitfield <brendanw@windworksdesign.com>
2015-12-10 03:13:54 +0000

    register a hwrng device, and report click time in nanoseconds

--- krad.c
+++ krad.c
@@ -20,13 +20,44 @@
 #include <linux/kernel.h>
 #include <linux/gpio.h>
 #include <linux/interrupt.h> 
+#include <linux/time.h>
+#include <linux/hw_random.h>
 
-/* Define GPIOs for BUTTONS */
+/* Define a GPIO for the Geiger counter */
 static int geiger_pulse_pin = 17; // listens for incoming pulses from the gieger counter
-
 
 /* Later on, the assigned IRQ numbers for the buttons are stored here */
 static int geiger_irq = -1;
+
+/* The last time of a click */
+static struct timespec t;
+
+
+static int geiger_data_read(struct hwrng* rng, u32 *data)
+{
+	*data = (u32) t.tv_nsec;
+	return 1;
+}
+
+/*
+static int geiger_read(struct hwrng* rng, void* data, size_t max, bool wait)
+{
+	return 0;
+}
+*/
+
+static struct hwrng geiger_rng = {
+	"Geiger Counter",
+	NULL,
+	NULL,
+	NULL,
+	geiger_data_read,
+	//geiger_read,
+	NULL,
+	0,
+	1
+};
+
 
 /*
  * The interrupt service routine called on geiger pulses
@@ -35,7 +66,8 @@ static irqreturn_t geiger_isr(int irq, void *data)
 {
 	if(irq == geiger_irq)
 	{
-		printk(KERN_INFO "pulse");
+		t = CURRENT_TIME;
+		//printk(KERN_INFO "pulse %ld", (long)t.tv_nsec);
 	}
 
 	return IRQ_HANDLED;
@@ -50,11 +82,11 @@ static int __init krad_init(void)
 
 	printk(KERN_INFO "%s\n", __func__);
 
-	// register BUTTON gpios
+	// register Geiger pulse gpio
 	ret = gpio_request_one(geiger_pulse_pin, GPIOF_IN, "Geiger Pulse");
 
 	if (ret) {
-		printk(KERN_ERR "Unable to request GPIOs for Geiger Counter: %d\n", ret);
+		printk(KERN_ERR "Unable to request GPIO for the Geiger Counter: %d\n", ret);
 		goto fail1;
 	}
 
@@ -71,18 +103,28 @@ static int __init krad_init(void)
 
 	printk(KERN_INFO "Successfully requested Geiger Pulse IRQ # %d\n", geiger_irq);
 
-	ret = request_irq(geiger_irq, geiger_isr, IRQF_TRIGGER_RISING, "gpiomod#button1", NULL);
+	ret = request_irq(geiger_irq, geiger_isr, IRQF_TRIGGER_RISING, "krad#geiger", NULL);
 
 	if(ret) {
 		printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
 		goto fail2;
 	}
 
+	ret = hwrng_register(&geiger_rng);
+
+	if(ret) {
+		printk(KERN_ERR "Unable to register hardware RNG device: %d\n", ret);
+		goto fail3;
+	}
+
+	printk(KERN_INFO "Successfully registered new hardware RNG device\n");
 
 	return 0;
 
 // cleanup what has been setup so far
 
+fail3:
+	free_irq(geiger_irq, NULL);
 fail2: 
 	gpio_free(geiger_pulse_pin);
 fail1:
@@ -95,6 +137,9 @@ fail1:
 static void __exit krad_exit(void)
 {
 	printk(KERN_INFO "%s\n", __func__);
+
+	// unregister the hwrng
+	hwrng_unregister(&geiger_rng);
 
 	// free irqs
 	free_irq(geiger_irq, NULL);
