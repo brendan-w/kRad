7f8fc0444c8a0e62510a38e06ecfaae29c8caa1f
Brendan Whitfield <brendanw@windworksdesign.com>
2015-12-10 02:08:44 +0000

    modified button example for geiger pulses

--- krad.c
+++ krad.c
@@ -3,6 +3,7 @@
  *
  * Author:
  * 	Stefan Wendler (devnull@kaltpost.de)
+ *      Brendan Whitfield (bcw7044@rit.edu)
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -20,30 +21,21 @@
 #include <linux/gpio.h>
 #include <linux/interrupt.h> 
 
-/* Define GPIOs for LEDs */
-static struct gpio leds[] = {
-		{  4, GPIOF_OUT_INIT_LOW, "LED 1" },
-};
-
 /* Define GPIOs for BUTTONS */
-static struct gpio buttons[] = {
-		{ 17, GPIOF_IN, "BUTTON 1" },	// turns LED on
-		{ 18, GPIOF_IN, "BUTTON 2" },	// turns LED off
-};
+static int geiger_pulse_pin = 17; // listens for incoming pulses from the gieger counter
+
 
 /* Later on, the assigned IRQ numbers for the buttons are stored here */
-static int button_irqs[] = { -1, -1 };
+static int geiger_irq = -1;
 
 /*
- * The interrupt service routine called on button presses
+ * The interrupt service routine called on geiger pulses
  */
-static irqreturn_t button_isr(int irq, void *data)
+static irqreturn_t geiger_isr(int irq, void *data)
 {
-	if(irq == button_irqs[0] && !gpio_get_value(leds[0].gpio)) {
-			gpio_set_value(leds[0].gpio, 1);
-	}
-	else if(irq == button_irqs[1] && gpio_get_value(leds[0].gpio)) {
-			gpio_set_value(leds[0].gpio, 0);
+	if(irq == geiger_irq)
+	{
+		printk(KERN_INFO "pulse");
 	}
 
 	return IRQ_HANDLED;
@@ -58,73 +50,42 @@ static int __init krad_init(void)
 
 	printk(KERN_INFO "%s\n", __func__);
 
-	// register LED gpios
-	ret = gpio_request_array(leds, ARRAY_SIZE(leds));
-
-	if (ret) {
-		printk(KERN_ERR "Unable to request GPIOs for LEDs: %d\n", ret);
-		return ret;
-	}
-	
 	// register BUTTON gpios
-	ret = gpio_request_array(buttons, ARRAY_SIZE(buttons));
+	ret = gpio_request_one(geiger_pulse_pin, GPIOF_IN, "Geiger Pulse");
 
 	if (ret) {
-		printk(KERN_ERR "Unable to request GPIOs for BUTTONs: %d\n", ret);
+		printk(KERN_ERR "Unable to request GPIOs for Geiger Counter: %d\n", ret);
 		goto fail1;
 	}
 
-	printk(KERN_INFO "Current button1 value: %d\n", gpio_get_value(buttons[0].gpio));
+	//printk(KERN_INFO "Current button1 value: %d\n", gpio_get_value(buttons[0].gpio));
 	
-	ret = gpio_to_irq(buttons[0].gpio);
+	ret = gpio_to_irq(geiger_pulse_pin);
 
 	if(ret < 0) {
 		printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
 		goto fail2;
 	}
 
-	button_irqs[0] = ret;
+	geiger_irq = ret;
 
-	printk(KERN_INFO "Successfully requested BUTTON1 IRQ # %d\n", button_irqs[0]);
+	printk(KERN_INFO "Successfully requested Geiger Pulse IRQ # %d\n", geiger_irq);
 
-	ret = request_irq(button_irqs[0], button_isr, IRQF_TRIGGER_RISING, "gpiomod#button1", NULL);
+	ret = request_irq(geiger_irq, geiger_isr, IRQF_TRIGGER_RISING, "gpiomod#button1", NULL);
 
 	if(ret) {
 		printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
 		goto fail2;
 	}
 
-
-	ret = gpio_to_irq(buttons[1].gpio);
-
-	if(ret < 0) {
-		printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
-		goto fail2;
-	}
-		
-	button_irqs[1] = ret;
-
-	printk(KERN_INFO "Successfully requested BUTTON2 IRQ # %d\n", button_irqs[1]);
-
-	ret = request_irq(button_irqs[1], button_isr, IRQF_TRIGGER_RISING, "gpiomod#button2", NULL);
-
-	if(ret) {
-		printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
-		goto fail3;
-	}
 
 	return 0;
 
 // cleanup what has been setup so far
-fail3:
-	free_irq(button_irqs[0], NULL);
 
 fail2: 
-	gpio_free_array(buttons, ARRAY_SIZE(leds));
-
+	gpio_free(geiger_pulse_pin);
 fail1:
-	gpio_free_array(leds, ARRAY_SIZE(leds));
-
 	return ret;	
 }
 
@@ -133,22 +94,13 @@ fail1:
  */
 static void __exit krad_exit(void)
 {
-	int i;
-
 	printk(KERN_INFO "%s\n", __func__);
 
 	// free irqs
-	free_irq(button_irqs[0], NULL);
-	free_irq(button_irqs[1], NULL);
-	
-	// turn all LEDs off
-	for(i = 0; i < ARRAY_SIZE(leds); i++) {
-		gpio_set_value(leds[i].gpio, 0); 
-	}
+	free_irq(geiger_irq, NULL);
 	
 	// unregister
-	gpio_free_array(leds, ARRAY_SIZE(leds));
-	gpio_free_array(buttons, ARRAY_SIZE(buttons));
+	gpio_free(geiger_pulse_pin);
 }
 
 MODULE_LICENSE("GPL");
